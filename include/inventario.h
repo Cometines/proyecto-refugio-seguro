3ba84034d979c01a8a84a90c6d10a565100c5b9e
Randy Rivera <ranrive42@gmail.com>
2026-04-10 11:41:41 -0600

    feat: incluir cabecera en el *.c

--- include/inventario.h
+++ include/inventario.h
@@ -16,4 +16,6 @@ void mostrarReporteInventario(Insumo inventario[]);
 // Función auxiliar para sumar o restar existencias (se usa al atender o al deshacer)
 void actualizarInsumo(Insumo inventario[], int id_insumo, int cantidad);
 
+//sixseven
+
 #endif
